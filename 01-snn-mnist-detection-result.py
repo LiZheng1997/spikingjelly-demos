0d276d72dbde546c49407dabdf76da6b41337daa
zhengli <1429053840@qq.com>
2024-07-03 19:34:26 +0800

    update codes

--- 01-snn-mnist-detection-result.py
+++ 01-snn-mnist-detection-result.py
@@ -1,17 +1,16 @@
 import torch
 import torch.nn as nn
 import numpy as np
-from spikingjelly.clock_driven import neuron
 from spikingjelly import visualizing
 from matplotlib import pyplot as plt
 import torchvision
 
 # 初始化一个MNIST数据测试集，
 test_dataset = torchvision.datasets.MNIST(
-    root='/home/lz/Documents/Programs-Released-Codes/Programming-Codes-Python/data/',
+    root='/home/lz/Documents', # 更改为你自己的MNIST数据集的绝对路径
     train=False,
     transform=torchvision.transforms.ToTensor(),
-    download=True
+    download=True # 如果你不需要自动帮你下载数据，则改为False
 )
 
 # 展示第一张图片
@@ -20,18 +19,16 @@ img = np.reshape(img, (28,28))
 plt.imshow(img)
 plt.axis('off')
 
-# 超参数T时间常量
-T = 100
-spikes_array = np.load("s_t_array.npy")
-voltage_array = np.load("v_t_array.npy")
 
-spikes_list = spikes_array.tolist()
-voltage_list = voltage_array.tolist()
+T = 100 # 超参数T时间常量
+#下面两个数组的数据来源于lif_fc_mnist.py脚本训练的模型推理得到的结果
+spikes_array = np.load("s_t_array.npy") # 读取spikes array的数据
+voltage_array = np.load("v_t_array.npy") # 读取voltage array的数据
 
-
-visualizing.plot_2d_heatmap(array=np.asarray(voltage_list), title='Membrane Potentials', xlabel='Simulating Step',
+# 可视化数据结果，heatmap和spikes
+visualizing.plot_2d_heatmap(array=voltage_array, title='Membrane Potentials', xlabel='Simulating Step',
                             ylabel='Neuron Index', int_x_ticks=True, x_max=T, dpi=200)
-visualizing.plot_1d_spikes(spikes=np.asarray(spikes_list), title='Membrane Potentials', xlabel='Simulating Step',
+visualizing.plot_1d_spikes(spikes=spikes_array, title='Membrane Potentials', xlabel='Simulating Step',
                            ylabel='Neuron Index', dpi=200)
 
 plt.show()
